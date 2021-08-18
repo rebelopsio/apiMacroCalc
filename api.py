646850d13f5d58ac10d546c68cd4c9711d119304
rebelopsio <steve@rebelops.io>
2021-08-18 14:10:39 -0700

    add python files

---
+++ api.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('sex', type=str)
+parser.add_argument('weight', type=int)
+parser.add_argument('heightinches', type=int)
+parser.add_argument('age', type=int)
+
+
+class apiMacroCalc(Resource):
+
+    def get(self):
+        req_data = {}
+        args = parser.parse_args()
+        req_data['sex'] = args['sex']
+        req_data['weight'] = args['weight']
+        req_data['height_inches'] = args['heightinches']
+        req_data['age'] = args['age']
+        return req_data, 201
+
+
+api.add_resource(apiMacroCalc, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
